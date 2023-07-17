727b6c9dacf6063986e684dafe6ac27f4eddf5fa
Allie King <alliek@sfei.org>
2023-07-17 23:52:19 +0000

    revamped run launchers to work better with both older and newer versions of dfm

--- run_launcher_part_0.sh
+++ run_launcher_part_0.sh
@@ -5,28 +5,27 @@
 ################################################
 
 # specify parameters for this run 
-export RUN_NAME="wy2022_r52184_new_stations" # name of the run (this will be name of *.mdu file and folder it's stored in)
+export RUN_NAME="wy2022_t140737_new_stations" # name of the run (this will be name of *.mdu file and folder it's stored in)
 export RUN_START="2021-08-01"  # run start time in YYYY-MM-DD format
 export RUN_STOP="2022-10-01"   # run end time in YYYY-MM-DD format
 export MAKE_PLOTS="True"       # flag to make and save plots of boundary conditions
-export SFB_DFM_PARENT_PATH=/boisevol1/hpcshared/open_bay/hydro/full_res/wy2022_r52184     # this is the directory where the sfb_dfm and stompy are located, and it is where the "runs" folder will be created
+export SFB_DFM_PARENT_PATH=/chicagovol1/hpcshared/open_bay/hydro/full_res/wy2022_t140737     # this is the directory where the sfb_dfm and stompy are located, and it is where the "runs" folder will be created
 
 # specify number of processors
 export NPROC=16
 
 # specify path to DFM 
-export DFM_PATH=/opt/software/delft/dfm/r52184-opt # path to DFM binaries and libraries (bin and lib folders should be inside here) 
+#export DFM_PATH=/opt/software/delft/dfm/r52184-opt # path to DFM binaries and libraries (bin and lib folders should be inside here) 
+export DFM_PATH=/opt/anaconda3/envs/dfm_t140737/
 
 # specify exact location of python and ddcouplefm/waqmerge executables
 export PYTHON=/opt/anaconda3/envs/delft_env/bin/python # path to python executable
 
-# these are the paths if the code is old (r52814)
-export DDCOUPLE_PATH=/opt/software/delft/ddcouplefm/1.02.01.50002/lnx64/ # path to ddcouplefm or waqmerge bin and lib directories
-export DDCOUPLE_NAME=ddcouplefm # this is ddcouplefm for older code and waqmerge for newer code
-
-# these are the paths if the code is newer
-#export DDCOUPLE_PATH=/opt/software/delft/dfm/t141798/
-#export DDCOUPLE_NAME=waqmerge
+# for the old code, point to ddcouplefm, and for the new code use waqmerge that comes from a newer distribution
+#export DDCOUPLE_PATH=/opt/software/delft/ddcouplefm/1.02.01.50002/lnx64/ # path to ddcouplefm or waqmerge bin and lib directories
+#export DDCOUPLE_NAME=ddcouplefm # this is ddcouplefm for older code and waqmerge for newer code
+export DDCOUPLE_PATH=/opt/software/delft/dfm/t141798/
+export DDCOUPLE_NAME=waqmerge
 
 ###############################################################################################################
 ##### the following don't need to be altered, provided user organizes folders in a certain way (see README.md)

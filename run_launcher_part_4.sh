0b1df09c4e677d6d1b1913a60ee85f4fbd803e4b
Allie King <alliek@sfei.org>
2023-07-18 20:58:02 +0000

    minor change

--- run_launcher_part_4.sh
+++ run_launcher_part_4.sh
@@ -10,7 +10,8 @@ source run_launcher_part_0.sh
 export PYTHONPATH=$STOMPY_PATH       
 
 # path to *.hyd file, should be based on run path and run name, if not enter whatever is correct
-HYDRO_PATH=$RUN_DIR/DFM_DELWAQ_$RUN_NAME/$RUN_NAME.hyd
+# this gets passed to the sfb_dfm_postprocessor.py script
+export HYDRO_PATH=$RUN_DIR/DFM_DELWAQ_$RUN_NAME/$RUN_NAME.hyd
 
 # change to run directory
 cd $RUN_DIR
